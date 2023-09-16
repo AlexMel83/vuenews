09a8c14a41f8732ab2cfc4fc3738a55378c44521
OleksandrZhelihovskyi <zhelihovskii.aleksandr@gmail.com>
2023-09-16 23:40:03 +0300

    last commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
+//here your bootstrap
 
 createApp(App).mount('#app');
