fbbaaacaeafe7adde52692a3705ef3d154bb1e00
OleksandrZhelihovskyi <zhelihovskii.aleksandr@gmail.com>
2023-09-16 23:29:26 +0300

    js added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 createApp(App).mount('#app');
 
